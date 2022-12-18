1c1204d9774a903a4e12e6fdfc56d6755749e86e
ethanforvest <ethanforvest@gmail.com>
2022-12-18 21:53:00 +0530

    can upload files and folders

--- telegram-cloud/telecloud.py
+++ telegram-cloud/telecloud.py
@@ -4,11 +4,17 @@ mode = "upload"
 name = "tele"
 user_name = "me"
 
-path = input("Enter the File Path: ")
-
-list_os = os.listdir(path)
-
-for x in list_os:
-    whole_path = path + "\\" + x
-    if os.path.isfile(whole_path):
-        os.system(f"tgcloud -m {mode} -n {name} -u {user_name} -p {whole_path}")
+while True:
+    path_input = input("Enter the File Path: ")
+    count = 0
+    if os.path.isfile(path_input):
+        os.system(f"tgcloud -m {mode} -n {name} -u {user_name} -p {path_input}")
+        count += 1
+    else:   
+        list_os = os.listdir(path_input)
+        for x in list_os:
+            whole_path = path_input + "\\" + x
+            if os.path.isfile(whole_path):
+                os.system(f"tgcloud -m {mode} -n {name} -u {user_name} -p \"{whole_path}\"")
+                count += 1
+    print(f"Total uploads: {count}")                
