b1e6b8856942e6b05e2d6422faabc57741d671a2
ethanforvest <ethanforvest@gmail.com>
2022-12-22 20:10:52 +0530

    spaces

--- telegram-cloud/telecloud.py
+++ telegram-cloud/telecloud.py
@@ -28,7 +28,7 @@ elif platform.system() == "Linux" or platform.system() == "Linux2":
     file_loc = "/"
     
 while True:
-    path_input = input("Enter the File Path: ")
+    path_input = input("Enter the File/Folder Path: ")
     count = 0
     if os.path.isfile(path_input):
         os.system(f"tgcloud -m {mode} -n {UNIQUE_NAME} -u {user_name} -p {path_input}")
@@ -43,11 +43,12 @@ while True:
         for x in list_os:
             whole_path = path_input + file_loc + x
             if os.path.isfile(whole_path):
-                print(f"Current Process --> {count + 1}/{list_os_files_len}")
+                print(f"Current Process --> {count + 1}/{list_os_files_len} \n")
                 os.system(f"tgcloud -m {mode} -n {UNIQUE_NAME} -u {user_name} -p \"{whole_path}\"")
                 file_size = os.path.getsize(whole_path)
                 total_size += file_size
                 count += 1
+                print("\n")
                 
     total_size_kb = total_size / 1024   
     total_size_mb = total_size_kb / 1024   
@@ -58,5 +59,6 @@ while True:
         total_size_mb = round(total_size_mb, 2)
         total_size = f"{total_size_mb} MB" 
     print(f"Total Uploads: {count} | Total Size: {total_size}")
-    total_size = 0      
+    total_size = 0  
+    print("\n")    
        
